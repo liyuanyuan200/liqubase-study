960fe258d162c27b95646d2af4be4f873a9cba91
yuanyuan <1250761608@qq.com>
2021-06-15 12:07:53 +0800

    提交测试

---
+++ src/main/java/com/yuan/hsbcliqubase/HsbcLiqubaseApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.hsbcliqubase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcLiqubaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsbcLiqubaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuan/hsbcliqubase/HsbcLiqubaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.hsbcliqubase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsbcLiqubaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
